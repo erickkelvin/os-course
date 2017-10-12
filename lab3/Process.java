3032ece7b77b9ca3b87ebfcac7cb5f9231973830
Erick Kelvin <erickkelvin@Ericks-MacBook-Pro.local>
2017-10-11 21:12:34 -0300

    adding process file and class

---
+++ lab3/Process.java
@@ -0,0 +1,30 @@
+public class Process {
+    public int id = -1;
+    public int arrivalTime = -1;
+    public int burstTime = -1;
+    public int priority = -1;
+
+    public int turnAround = -1;
+    public int waitingTime = -1;
+    public int responseTime = -1;
+
+    public Process(int id, int arrivalTime, int burstTime, int priority) {
+        this.id = id;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+        this.priority = priority;
+        this.turnAround = 0;
+        this.waitingTime = 0;
+        this.responseTime = -1;
+    }
+
+    public Process(Process p) {
+        this.id = p.id;
+        this.arrivalTime = p.arrivalTime;
+        this.burstTime = p.burstTime;
+        this.priority = p.priority;
+        this.turnAround = p.turnAround;
+        this.waitingTime = p.waitingTime;
+        this.responseTime = p.responseTime;
+    }
+}
